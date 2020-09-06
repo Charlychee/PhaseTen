6f811c104d2078bcebd105c09a35d424ce5e73f0
Charlychee <charles.lychee@gmail.com>
2020-09-05 23:45:44 -0700

    Implemented members of card class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,32 @@
 public class Card {
+    enum typeEnum {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        WILD,
+        SKIP
+    }
+
+    private final typeEnum type;
+    private final int value;
+
+    public Card(typeEnum _color, int _value) {
+        type = _color;
+        value = _value;
+    }
+
+    public typeEnum getType() {
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String toString()
+    {
+        return "Color: " + type + ", Value: " + value;
+    }
+
 }
