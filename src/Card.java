7a1d51a812fd455945f495e9e7dac99b4a2bbcd4
Charlychee <charles.lychee@gmail.com>
2020-09-06 18:56:30 -0700

    Implemented PlayerCardPile methods

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,9 @@
-public class Card {
+public class Card implements Comparable<Card>{
+    @Override
+    public int compareTo(Card o) {
+        return Integer.compare(value, o.value);
+    }
+
     enum typeEnum {
         RED,
         GREEN,
--- src/PlayerCardPile.java
+++ src/PlayerCardPile.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -8,18 +9,21 @@ public class PlayerCardPile extends CardPile {
         RUN,
         EVEN_ODD
     }
-    static final HashMap<Integer, String> colors = new HashMap<>() {
+    static final HashMap<Card.typeEnum, Integer> colors = new HashMap<>() {
         {
-            put(0, "Blue");
-            put(1, "Green");
-            put(2, "Red");
-            put(3, "Yellow");
+            put(Card.typeEnum.BLUE, 0);
+            put(Card.typeEnum.GREEN, 1);
+            put(Card.typeEnum.RED, 2);
+            put(Card.typeEnum.YELLOW, 3);
         }
     };
     final typeEnum type;
-    int typeData;
+    int typeData = 0;
 
     public PlayerCardPile(typeEnum _type, LinkedList<Card> _cards) throws Exception {
+        if(_cards.isEmpty()) {
+            throw new Exception();
+        }
         cards = _cards;
         size = _cards.size();
         type = _type;
@@ -30,37 +34,54 @@ public class PlayerCardPile extends CardPile {
                 }
                 break;
             case NUM_SET:
+                typeData = _cards.get(0).getValue();
                 if(!isNumSet()) {
                     throw new Exception();
                 }
                 break;
             case EVEN_ODD:
-                if(!isEven() || !isOdd()) {
+                typeData = _cards.get(0).getValue() % 2;
+                if(!isEvenOdd()) {
                     throw new Exception();
                 }
                 break;
             case COLOR_SET:
+                typeData = colors.get(_cards.get(0).getType());
                 if(!isColorSet()) {
                     throw new Exception();
                 }
                 break;
         }
-
     }
 
     private boolean isRun(){
+        Collections.sort(cards);
+        int expected = cards.get(0).getValue();
+        for(Card card : cards) {
+            if(card.getValue() != expected) {
+                return false;
+            }
+            ++expected;
+        }
         return true;
     }
     private boolean isNumSet() {
+        for(Card card : cards) {
+            if(card.getValue() != typeData) {
+                return false;
+            }
+        }
         return true;
     }
     private boolean isColorSet() {
-        return true;
+        return false;
     }
-    private boolean isEven() {
-        return true;
-    }
-    private boolean isOdd() {
+    private boolean isEvenOdd() {
+        for(Card card : cards) {
+            if(card.getValue() % 2 != typeData) {
+                return false;
+            }
+        }
         return true;
     }
 
