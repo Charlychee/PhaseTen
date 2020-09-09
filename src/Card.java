c5129f9ed79d54c8ee0b3f3177c501d28456734f
Charlychee <charles.lychee@gmail.com>
2020-09-08 22:47:22 -0700

    Added documentation and extra protection

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,12 @@
 public class Card implements Comparable<Card>{
+
+    /** Compares the values of the cards*/
     @Override
     public int compareTo(Card o) {
         return Integer.compare(value, o.value);
     }
 
+    /** Represents the type of card*/
     enum typeEnum {
         RED,
         GREEN,
@@ -13,25 +16,70 @@ public class Card implements Comparable<Card>{
         SKIP
     }
 
+    /** Holds the type of card*/
     private final typeEnum type;
+
+    /** Holds the value of the card*/
     private final int value;
 
-    public Card(typeEnum _color, int _value) {
-        type = _color;
-        value = _value;
+    /** The minimum value of a color card*/
+    private final static int minValue = 1;
+    /** The maximum value of a color card*/
+    private final static int maxValue = 12;
+
+    /**
+     * Sets up card color and value. For color cards.
+     * @param _type The color of the card
+     * @param _value The value of the card
+     */
+    public Card(typeEnum _type, int _value) throws Exception {
+        if (_type != typeEnum.WILD && _type != typeEnum.SKIP
+                && _value <= maxValue && _value >= minValue) {
+            type = _type;
+            value = _value;
+        }
+        else throw new Exception("Invalid Card");
     }
 
+    /**
+     * Sets up card type and value. For WILD or SKIP cards only.
+     * @param _type The type of the card
+     * @throws Exception Card must be WILD or SKIP
+     */
+    public Card(typeEnum _type) throws Exception {
+        if (_type == typeEnum.WILD || _type == typeEnum.SKIP) {
+            type = _type;
+            value = -1;
+        }
+        else throw new Exception("Invalid Card");
+    }
+
+    /**
+     * Returns the type
+     * @return The type of the card
+     */
     public typeEnum getType() {
         return type;
     }
 
+    /**
+     * Returns the value
+     * @return The value of the card
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Prints the color and value
+     * @return A string of color and value of the Card
+     */
     public String toString()
     {
-        return "Color: " + type + ", Value: " + value;
+        if (type != typeEnum.WILD && type != typeEnum.SKIP)
+            return "Color: " + type + ", Value: " + value;
+        else
+            return "Type: " + type;
     }
 
 }
--- src/DrawDeck.java
+++ src/DrawDeck.java
@@ -1,10 +1,8 @@
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 
 public class DrawDeck extends CardPile{
     /** Creates the standard starting deck. */
-    public DrawDeck() {
+    public DrawDeck() throws Exception {
         Card.typeEnum[] colors = new Card.typeEnum[]
                 {
                         Card.typeEnum.BLUE,
@@ -12,10 +10,10 @@ public class DrawDeck extends CardPile{
                         Card.typeEnum.RED,
                         Card.typeEnum.YELLOW
                 };
-        for (int i = 0; i < colors.length; i += 1) {
+        for (Card.typeEnum color : colors) {
             for (int k = 1; k <= 12; k += 1) {
-                cards.add(new Card(colors[i], k));
-                cards.add(new Card(colors[i], k));
+                cards.add(new Card(color, k));
+                cards.add(new Card(color, k));
             }
             cards.add(new Card(Card.typeEnum.SKIP, 0));
             cards.add(new Card(Card.typeEnum.WILD, 0));
