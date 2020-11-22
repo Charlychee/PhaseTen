64ba3edf1e0bdf4e8277fe7d28743327f361d0d0
Stephanie Trinh <stephanie_trinh@berkeley.edu>
2020-11-22 11:51:03 -0800

    Finished creating all the phases for the original PhaseTen game and created tests for the phases.

---
+++ src/UnitTests/PhaseTest.java
@@ -0,0 +1,79 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.Phase.ORIGINAL_PHASES;
+import static org.junit.Assert.*;
+
+public class PhaseTest {
+    protected static final ArrayList<Integer> EMPTY = new ArrayList<>();
+    protected static final ArrayList<Integer> ONEFOUR = new ArrayList<>(Arrays.asList(4));
+    protected static final ArrayList<Integer> ONESEVEN = new ArrayList<>(Arrays.asList(7));
+
+    @Test
+    public void testOriginalPhases() {
+        assertEquals(new ArrayList<>(Arrays.asList(3, 3)), ORIGINAL_PHASES[0].getNumSets());
+        assertEquals(EMPTY, ORIGINAL_PHASES[0].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[0].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[0].getColorSets());
+        assertEquals("2 number set(s) of size 3, 3", ORIGINAL_PHASES[0].toString());
+
+        assertEquals(new ArrayList<>(Arrays.asList(3)), ORIGINAL_PHASES[1].getNumSets());
+        assertEquals(ONEFOUR, ORIGINAL_PHASES[1].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[0].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[0].getColorSets());
+        assertEquals("1 number set(s) of size 3\n1 run(s) of size 4", ORIGINAL_PHASES[1].toString());
+
+        assertEquals(ONEFOUR, ORIGINAL_PHASES[2].getNumSets());
+        assertEquals(ONEFOUR, ORIGINAL_PHASES[2].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[2].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[2].getColorSets());
+        assertEquals("1 number set(s) of size 4\n1 run(s) of size 4", ORIGINAL_PHASES[2].toString());
+
+        assertEquals(EMPTY, ORIGINAL_PHASES[3].getNumSets());
+        assertEquals(ONESEVEN, ORIGINAL_PHASES[3].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[3].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[3].getColorSets());
+        assertEquals("1 run(s) of size 7", ORIGINAL_PHASES[3].toString());
+
+        assertEquals(EMPTY, ORIGINAL_PHASES[4].getNumSets());
+        assertEquals(new ArrayList<>(Arrays.asList(8)), ORIGINAL_PHASES[4].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[4].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[4].getColorSets());
+        assertEquals("1 run(s) of size 8", ORIGINAL_PHASES[4].toString());
+
+        assertEquals(EMPTY, ORIGINAL_PHASES[5].getNumSets());
+        assertEquals(new ArrayList<>(Arrays.asList(9)), ORIGINAL_PHASES[5].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[5].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[5].getColorSets());
+        assertEquals("1 run(s) of size 9", ORIGINAL_PHASES[5].toString());
+
+        assertEquals(new ArrayList<>(Arrays.asList(4, 4)), ORIGINAL_PHASES[6].getNumSets());
+        assertEquals(EMPTY, ORIGINAL_PHASES[6].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[6].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[6].getColorSets());
+        assertEquals("2 number set(s) of size 4, 4", ORIGINAL_PHASES[6].toString());
+
+        assertEquals(EMPTY, ORIGINAL_PHASES[7].getNumSets());
+        assertEquals(EMPTY, ORIGINAL_PHASES[7].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[7].getEvenOdds());
+        assertEquals(ONESEVEN, ORIGINAL_PHASES[7].getColorSets());
+        assertEquals("1 color set(s) of size 7", ORIGINAL_PHASES[7].toString());
+
+        assertEquals(new ArrayList<>(Arrays.asList(2, 5)), ORIGINAL_PHASES[8].getNumSets());
+        assertEquals(EMPTY, ORIGINAL_PHASES[8].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[8].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[8].getColorSets());
+        assertEquals("2 number set(s) of size 2, 5", ORIGINAL_PHASES[8].toString());
+
+        assertEquals(new ArrayList<>(Arrays.asList(3, 5)), ORIGINAL_PHASES[9].getNumSets());
+        assertEquals(EMPTY, ORIGINAL_PHASES[9].getRuns());
+        assertEquals(EMPTY, ORIGINAL_PHASES[9].getEvenOdds());
+        assertEquals(EMPTY, ORIGINAL_PHASES[9].getColorSets());
+        assertEquals("2 number set(s) of size 3, 5", ORIGINAL_PHASES[9].toString());
+
+    }
+}
+
--- src/com/Phase.java
+++ src/com/Phase.java
@@ -2,19 +2,21 @@ package com;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Phase {
+    /** The ten phases of the original Phase 10 game. */
     public static final Phase[] ORIGINAL_PHASES = {
-            new Phase(new int[]{}, new int[]{3, 3}, new int[]{}),
-            new Phase(new int[]{4}, new int[]{3}, new int[]{}),
-            new Phase(new int[]{4}, new int[]{4}, new int[]{}),
-            new Phase(new int[]{7}, new int[]{}, new int[]{}),
-            new Phase(new int[]{8}, new int[]{}, new int[]{}),
-            new Phase(new int[]{9}, new int[]{}, new int[]{}),
-            new Phase(new int[]{}, new int[]{4, 4}, new int[]{}),
-            new Phase(new int[]{}, new int[]{}, new int[]{7}),
-            new Phase(new int[]{}, new int[]{5, 2}, new int[]{}),
-            new Phase(new int[]{}, new int[]{5, 3}, new int[]{}),
+            new Phase(2, 3, 3, 0, 0, 0),
+            new Phase(1, 3, 1, 4, 0, 0),
+            new Phase(1, 4, 1, 4, 0, 0),
+            new Phase(0, 1, 7, 0, 0),
+            new Phase(0, 1, 8, 0, 0),
+            new Phase(0, 1, 9, 0, 0),
+            new Phase(2, 4, 4, 0, 0, 0),
+            new Phase(0, 0, 0, 1, 7),
+            new Phase(2, 2, 5, 0, 0, 0),
+            new Phase(2, 3, 5, 0, 0, 0),
     };
 
     /** Information about the runs needed in this phase. */
@@ -29,30 +31,66 @@ public class Phase {
     /** Information about the color sets needed in this phase. */
     private ArrayList<Integer> colorSets;
 
-    Phase(int... numbers) {
-        int totalRuns = numbers[0];
-        int numSetIndex = 1 + totalRuns;
-        int totalNumSets = numbers[numSetIndex];
-        int evenOddIndex = 1 + numSetIndex + totalNumSets;
+    /**
+     *  Creates a phase with the given number of numberSets, runs, evenOdds, and colorSets.
+     *  The first argument gives the number of numberSets n, followed by n arguments specifying
+     *  the sizes of each numberSet. The remaining arguments follow the same pattern for runs,
+     *  evenOdds, and colorSets.
+     *  */
+    public Phase(Integer... numbers) {
+        int totalNumSets = numbers[0];
+        int runIndex = 1 + totalNumSets;
+        int totalRuns = numbers[runIndex];
+        int evenOddIndex = 1 + runIndex + totalRuns;
         int totalEvenOdds = numbers[evenOddIndex];
         int colorSetIndex = 1 + evenOddIndex + totalEvenOdds;
         int totalColorSets = numbers[colorSetIndex];
-        runs = (ArrayList)Arrays.asList(Arrays.copyOfRange(numbers, 1, totalRuns + 1));
-        numSets = (ArrayList)Arrays.asList(Arrays.copyOfRange(numbers, numSetIndex + 1, numSetIndex + 1 + totalNumSets));
-        evenOdds = (ArrayList)Arrays.asList(Arrays.copyOfRange(numbers, evenOddIndex + 1, evenOddIndex + 1 + totalEvenOdds));
-        colorSets = (ArrayList)Arrays.asList(Arrays.copyOfRange(numbers, colorSetIndex + 1, colorSetIndex + 1 + totalColorSets));
+        numSets = new ArrayList<>();
+        Collections.addAll(numSets, Arrays.copyOfRange(numbers, 1, totalNumSets + 1));
+        runs = new ArrayList<>();
+        Collections.addAll(runs, Arrays.copyOfRange(numbers, runIndex + 1, runIndex + 1 + totalRuns));
+        evenOdds = new ArrayList<>();
+        Collections.addAll(evenOdds, Arrays.copyOfRange(numbers, evenOddIndex + 1, evenOddIndex + 1 + totalEvenOdds));
+        colorSets = new ArrayList<>();
+        Collections.addAll(colorSets, Arrays.copyOfRange(numbers, colorSetIndex + 1, colorSetIndex + 1 + totalColorSets));
     }
 
-    public Phase(int[] _runs, int[] _numSets, int[] _evenOdds, int[] _colorSets) {
-        runs = (ArrayList)Arrays.asList(_runs);
-        numSets = (ArrayList)Arrays.asList(_numSets);
-        evenOdds = (ArrayList)Arrays.asList(_evenOdds);
-        colorSets = (ArrayList)Arrays.asList(_colorSets);
+    /** Returns a string explaining the necessary player card piles for this phase. */
+    public String toString() {
+        String phaseString = "";
+        if (numSets.size() != 0) {
+            phaseString += numSets.size() + " number set(s) of size " + numSets.toString() +"\n";
+        }
+        if (runs.size() != 0) {
+            phaseString += runs.size() + " run(s) of size " + runs.toString() + "\n";
+        }
+        if (evenOdds.size() != 0) {
+            phaseString += evenOdds.size() + " even/odd(s) of size " + evenOdds.toString() + "\n";
+        }
+        if (colorSets.size() != 0) {
+            phaseString += colorSets.size() + " color set(s) of size " + colorSets.toString();
+        }
+        phaseString = phaseString.replaceAll("[\\[\\]]", "");
+        return phaseString.trim();
     }
 
-    public Phase(int[] _runs, int[] _numSets, int[] _colorSets) {
-        runs = (ArrayList)Arrays.asList(_runs);
-        numSets = (ArrayList)Arrays.asList(_numSets);
-        colorSets = (ArrayList)Arrays.asList(_colorSets);
+    /** Returns a copy of the arraylist specifying the runs needed for this phase. */
+    public ArrayList<Integer> getRuns() {
+        return new ArrayList<>(runs);
+    }
+
+    /** Returns a copy of the arraylist specifying the numSets needed for this phase. */
+    public ArrayList<Integer> getNumSets() {
+        return new ArrayList<>(numSets);
+    }
+
+    /** Returns a copy of the arraylist specifying the evenOdds needed for this phase. */
+    public ArrayList<Integer> getEvenOdds() {
+        return new ArrayList<>(evenOdds);
+    }
+
+    /** Returns a copy of the arraylist specifying the colorSets needed for this phase. */
+    public ArrayList<Integer> getColorSets() {
+        return new ArrayList<>(colorSets);
     }
 }
